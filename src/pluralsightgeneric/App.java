13b513a3b6e19ca03233b71c4da089264e56c823
arjunarjun27 <arjunarjun27@gmail.com>
2018-06-29 08:47:28 +0530

    mplementing a Generic Type

---
+++ src/pluralsightgeneric/App.java
@@ -0,0 +1,32 @@
+package pluralsightgeneric;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person donDraper =new Person("donDraper", 89);
+		
+		Person peggyOlson =new Person("peggyOlson", 69);
+		
+		Person bertCooper =new Person("bertCooper", 100);
+		
+		
+		List<Person> list=new ArrayList<Person>();
+		list.add(donDraper);
+		list.add(peggyOlson);
+		list.add(bertCooper);
+		
+		
+		Collections.sort(list,new AgeComparator());
+		
+		System.out.println(list);
+
+
+	}
+
+}
